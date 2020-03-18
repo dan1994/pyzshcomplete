2b246e762e1a5ec6539edfeee5ff9f1158f24267
Dan Arad <dan1994@gmail.com>
2020-03-18 22:44:03 +0200

    Restored properties (only needed to declare theproperty decorator above the abstractmethod one)

--- pyzshcomplete/adapters/argparse/argument_adapter.py
+++ pyzshcomplete/adapters/argparse/argument_adapter.py
@@ -11,28 +11,35 @@ from pyzshcomplete.adapters.base.argument_adapter import \
 
 class ArgparseArgumentAdapter(StringifyableArgumentAdapter):
 
+    @property
     def is_optional(self):
         return len(self._argument.option_strings) > 0
 
+    @property
     def name(self):
         return self._argument.dest
 
+    @property
     def options(self):
-        if self.is_positional():
+        if self.is_positional:
             raise TypeError('A positional argument doesn\'t have options')
         return self._argument.option_strings
 
+    @property
     def subargument_count(self):
         if self._argument.nargs is None:
             return 1
         return self._argument.nargs
 
+    @property
     def help(self):
         return self._argument.help
 
+    @property
     def is_exclusive(self):
         return isinstance(self._argument, (_HelpAction, _VersionAction))
 
+    @property
     def can_repeat(self):
         return isinstance(self._argument,
                           (_AppendAction, _AppendConstAction, _ExtendAction))
