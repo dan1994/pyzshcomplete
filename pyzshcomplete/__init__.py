2396c34e8ef132d3085d11c9259a292662918852
Dan Arad <dan1994@gmail.com>
2020-03-18 12:07:01 +0200

    Moved ZshCompleter logic to autocomplete function since it's simple enough

--- pyzshcomplete/__init__.py
+++ pyzshcomplete/__init__.py
@@ -1,18 +1,23 @@
 from os import environ
+from sys import exit
 
-from pyzshcomplete.zsh_completer import ZshCompleter
+from pyzshcomplete.adapters.argparse.parser_adapter import ArgparseParserAdapter
 
 
 __all__ = ['autocomplete']
 
 
 def autocomplete(parser):
-	if not _running_in_autocompletion_context():
-		return
+    if not _running_in_autocompletion_context():
+        return
 
-	completer = ZshCompleter(parser)
-	completer.complete()
+    parser = ArgparseParserAdapter(parser)
+    arguments_as_string = str(parser)
+    print(arguments_as_string)
+
+    found_arguments = 0 if len(arguments_as_string) > 0 else -1
+    exit(found_arguments)
 
 
 def _running_in_autocompletion_context():
-	return 'ARGCOMPLETE' in environ
+    return 'ARGCOMPLETE' in environ
