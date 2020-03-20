14e072c37fe90dbd6301e34eedcd754269c57833
Dan Arad <dan1994@gmail.com>
2020-03-20 17:13:49 +0200

    Small refactor to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,29 +4,31 @@ from io import StringIO
 import os
 import sys
 
-sys.path.append('../pyzshcomplete')
 from pyzshcomplete import autocomplete
+
+
+sys.path.append('../pyzshcomplete')
 
 os.environ['PYZSHCOMPLETE'] = '1'
 
 
 @fixture(scope='function')
 def default_parser():
-	return ArgumentParser()
+    return ArgumentParser()
 
 
 @fixture(scope='function')
 def empty_parser():
-	return ArgumentParser(add_help=False)
+    return ArgumentParser(add_help=False)
 
 
 @fixture(scope='function')
 def autocomplete_and_compare(capsys):
-	def _autocomplete_and_compare(parser, expected):
-		with raises(SystemExit):
-			autocomplete(parser)
-		output = capsys.readouterr().out
-		options = output.split('\n')[:-1]
-		assert sorted(options) == sorted(expected)
+    def _autocomplete_and_compare(parser, expected):
+        with raises(SystemExit):
+            autocomplete(parser)
+        output = capsys.readouterr().out
+        options = output.split('\n')[:-1]
+        assert sorted(options) == sorted(expected)
 
-	return _autocomplete_and_compare
+    return _autocomplete_and_compare
