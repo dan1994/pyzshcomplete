1b8d444b74cfb64838ddbbf3b9f34ec3f5e423cc
Dan Arad <dan1994@gmail.com>
2020-03-21 13:40:40 +0200

    Enhanced completion logic to use either a list of choices or zsh tags; In argparse, with no choices it currently always uses the _default tag; Added ZshConstants class for all tags and other magic strings

--- pyzshcomplete/adapters/base/argument_adapter.py
+++ pyzshcomplete/adapters/base/argument_adapter.py
@@ -61,14 +61,20 @@ class ArgumentAdapterInterface(metaclass=ABCMeta):
     def is_rest_of_arguments(self):
         return
 
+    @property
+    @abstractmethod
+    def complete_with(self):
+        return
+
+    @property
+    @abstractmethod
+    def completion_choices(self):
+        return
+
 
 class ArgumentAdapter(ArgumentAdapterInterface):
 
-    SUBARGUMENT_SPACE_SEPERATOR = ''
-    SUBARGUMENT_NO_SEPARATOR = '-'
-    SUBARGUMENT_EITHER_NO_OR_SPACE_SEPARATOR = '+'
-    SUBARGUMENT_EITHER_EQUAL_SIGN_OR_SPACE_SEPERATOR = '='
-    SUBARGUMENT_EITHER_EQUAL_SIGN_SEPERATOR = '=-'
+    COMPLETE_WITH_CHOICES = 'choices'
 
     def __str__(self):
         try:
@@ -95,7 +101,8 @@ class ArgumentAdapter(ArgumentAdapterInterface):
         completions = self._completions_to_string()
 
         if self.is_rest_of_arguments:
-            return '*:{message}:{action}'.format(
+            return '{variable_subarguments}:{message}:{action}'.format(
+                variable_subarguments=ZshConstants.VARIABLE_SUBARGUMENTS,
                 message=message,
                 action=completions
             )
@@ -109,14 +116,19 @@ class ArgumentAdapter(ArgumentAdapterInterface):
 
     def _exclusion_list_to_string(self):
         if self.is_exclusive:
-            return '(* -)'
+            all_exclusions = '{} {} {}'.format(
+                ZshConstants.Exclusion.REST_OF_ARGUMENTS,
+                ZshConstants.Exclusion.POSITIONAL,
+                ZshConstants.Exclusion.FLAGS
+            )
+            return '({})'.format(all_exclusions)
         # TODO - Missing exclusion of exclusive options for below cases
         if self.can_repeat:
             return ''
         return '({})'.format(' '.join(self.flags))
 
     def _can_repeat_to_string(self):
-        return '*' if self.can_repeat else ''
+        return ZshConstants.REPEATING_ARGUMENT if self.can_repeat else ''
 
     def _flags_to_string(self):
         self._validate_flags_prefix()
@@ -125,7 +137,8 @@ class ArgumentAdapter(ArgumentAdapterInterface):
     def _validate_flags_prefix(self):
         prefixes = map(lambda flag: flag[0], self.flags)
         bad_prefixes = list(
-            filter(lambda prefix: prefix not in ['-', '+'], prefixes))
+            filter(lambda prefix: prefix not in ZshConstants.SUPPORTED_PREFIXES,
+                   prefixes))
 
         if len(bad_prefixes) > 0:
             raise ValueError('Flag uses the {} prefix that is not supported by '
@@ -134,8 +147,8 @@ class ArgumentAdapter(ArgumentAdapterInterface):
 
     def _subargument_separator_to_string(self):
         if self.subargument_count == 1:
-            return ArgumentAdapter.SUBARGUMENT_EITHER_NO_OR_SPACE_SEPARATOR
-        return ArgumentAdapter.SUBARGUMENT_SPACE_SEPERATOR
+            return ZshConstants.SubargumentSeparator.NONE_OR_SPACE
+        return ZshConstants.SubargumentSeparator.SPACE
 
     def _help_to_string(self):
         if len(self.help) > 0:
@@ -143,7 +156,7 @@ class ArgumentAdapter(ArgumentAdapterInterface):
         return ''
 
     def _is_optional_to_string(self):
-        return ':' if self.is_optional else ''
+        return ZshConstants.OPTIONAL_ARGUMENT if self.is_optional else ''
 
     def _name_and_help_to_string(self):
         if len(self.help) > 0:
@@ -157,9 +170,151 @@ class ArgumentAdapter(ArgumentAdapterInterface):
                 self._completions_to_string()
             )
 
-    def _completions_to_string(self):
-        # TODO - Implement more complicated completers such as choice completer
-        return '_files'
-
     def _has_variable_subarguments_to_string(self):
-        return '*' if self.is_rest_of_arguments else ''
+        return ZshConstants.VARIABLE_SUBARGUMENTS if self.is_rest_of_arguments \
+            else ''
+
+    def _completions_to_string(self):
+        if self.complete_with == ArgumentAdapter.COMPLETE_WITH_CHOICES:
+            return self._choices_to_string()
+        return self._zsh_tags_to_string()
+
+    def _choices_to_string(self):
+        completion_choices = self._choices_to_dict()
+        completion_choices_as_string = ' '.join([
+            ArgumentAdapter._choice_to_string(choice, description)
+            for choice, description in completion_choices])
+        return '({})'.format(completion_choices_as_string)
+
+    def _choices_to_dict(self):
+        if isinstance(self.completion_choices, dict):
+            return self.completion_choices
+        if isinstance(self.completion_choices, (list, tuple, set)):
+            return {completion: None for completion in self.completion_choices}
+        return {self.completion_choices: None}
+
+    @staticmethod
+    def _choice_to_string(choice, description):
+        if description is None:
+            choice_as_string = str(choice)
+        else:
+            choice_as_string = '{}:{}'.format(choice, description)
+
+        escaped_choice_as_string = choice_as_string.replace(
+            ':', r'\:').replace(' ', r'\ ')
+
+        return escaped_choice_as_string
+
+    def _zsh_tags_to_string(self):
+        complete_with = self.complete_with
+        if not isinstance(self.complete_with, (list, tuple, set)):
+            complete_with = [complete_with]
+
+        complete_with = map(lambda tag: '_{}'.format(tag), complete_with)
+        return ' '.join(complete_with)
+
+
+class ZshConstants:
+
+    SUPPORTED_PREFIXES = ['+', '-']
+
+    OPTIONAL_ARGUMENT = ':'
+    REPEATING_ARGUMENT = '*'
+    VARIABLE_SUBARGUMENTS = '*'
+
+    class Exclusion:
+        REST_OF_ARGUMENTS = '*'
+        POSITIONAL = ':'
+        FLAGS = '-'
+
+    class SubargumentSeparator:
+        NO_SEPARATOR = '-'
+        SPACE = ''
+        EQUAL_SIGN = '=-'
+        NONE_OR_SPACE = '+'
+        EQUAL_SIGN_OR_SPACE = '='
+
+    class Tags:
+        ACCOUNTS = 'accounts'
+        ALL_EXPANSIONS = 'all-expansions'
+        ALL_FILES = 'all-files'
+        ARGUMENTS = 'arguments'
+        ARRAYS = 'arrays'
+        ASSOCIATION_KEYS = 'association-keys'
+        BOOKMARKS = 'bookmarks'
+        BUILTINS = 'builtins'
+        CHARACTERS = 'characters'
+        COLORMAPIDS = 'colormapids'
+        COLORS = 'colors'
+        COMMANDS = 'commands'
+        CONTEXTS = 'contexts'
+        CORRECTIONS = 'corrections'
+        CURSORS = 'cursors'
+        DEFAULT = 'default'
+        DESCRIPTIONS = 'descriptions'
+        DEVICES = 'devices'
+        DIRECTORIES = 'directories'
+        DIRECTORY_STACK = 'directory-stack'
+        DISPLAYS = 'displays'
+        DOMAINS = 'domains'
+        EXPANSIONS = 'expansions'
+        FILE_DESCRIPTORS = 'file-descriptors'
+        FILES = 'files'
+        FONTS = 'fonts'
+        FSTYPES = 'fstypes'
+        FUNCTIONS = 'functions'
+        GLOBBED_FILES = 'globbed-files'
+        GROUPS = 'groups'
+        HISTORY_WORDS = 'history-words'
+        HOSTS = 'hosts'
+        INDEXES = 'indexes'
+        JOBS = 'jobs'
+        INTERFACES = 'interfaces'
+        KEYMAPS = 'keymaps'
+        KEYSYMS = 'keysyms'
+        LIBRARIES = 'libraries'
+        LIMITS = 'limits'
+        LOCAL_DIRECTORIES = 'local-directories'
+        MANUALS = 'manuals'
+        MAILBOXES = 'mailboxes'
+        MAPS = 'maps'
+        MESSAGES = 'messages'
+        MODIFIERS = 'modifiers'
+        MODULES = 'modules'
+        MY_ACCOUNTS = 'my-accounts'
+        NAMED_DIRECTORIES = 'named-directories'
+        NAMES = 'names'
+        NEWSGROUPS = 'newsgroups'
+        NICKNAMES = 'nicknames'
+        OPTIONS = 'options'
+        ORIGINAL = 'original'
+        OTHER_ACCOUNTS = 'other-accounts'
+        PACKAGES = 'packages'
+        PARAMETERS = 'parameters'
+        PATH_DIRECTORIES = 'path-directories'
+        PATHS = 'paths'
+        PODS = 'pods'
+        PORTS = 'ports'
+        PREFIXES = 'prefixes'
+        PRINTERS = 'printers'
+        PROCESSES = 'processes'
+        PROCESSES_NAMES = 'processes-names'
+        SEQUENCES = 'sequences'
+        SESSIONS = 'sessions'
+        SIGNALS = 'signals'
+        STRINGS = 'strings'
+        STYLES = 'styles'
+        SUFFIXES = 'suffixes'
+        TAGS = 'tags'
+        TARGETS = 'targets'
+        TIME_ZONES = 'time-zones'
+        TYPES = 'types'
+        URLS = 'urls'
+        USERS = 'users'
+        VALUES = 'values'
+        VARIANT = 'variant'
+        VISUALS = 'visuals'
+        WARNINGS = 'warnings'
+        WIDGETS = 'widgets'
+        WINDOWS = 'windows'
+        ZSH_OPTIONS = 'zsh-options'
--- tests/test_completion.py
+++ tests/test_completion.py
@@ -4,18 +4,18 @@ def test_empty(empty_parser, autocomplete_and_compare):
 
 def test_default(default_parser, autocomplete_and_compare):
 	autocomplete_and_compare(default_parser, [
-		r'(* -){-h,--help}[show this help message and exit]'
+		r'(* : -){-h,--help}[show this help message and exit]'
 	])
 
 
 def test_positional(empty_parser, autocomplete_and_compare):
 	empty_parser.add_argument('pos', help='A positional argument')
 	autocomplete_and_compare(
-		empty_parser, [r':pos - A positional argument:_files'])
+		empty_parser, [r':pos - A positional argument:_default'])
 
 
 def test_optional(empty_parser, autocomplete_and_compare):
 	empty_parser.add_argument('-o', help='An optional argument')
 	autocomplete_and_compare(empty_parser, [
-		r'(-o){-o}+[An optional argument]: :_files'
+		r'(-o){-o}+[An optional argument]: :_default'
 	])
