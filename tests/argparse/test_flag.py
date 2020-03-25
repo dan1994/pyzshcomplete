929d4bcf966e19fa6d3b53e1b987e7604a29f036
Dan Arad <dan1994@gmail.com>
2020-03-25 22:06:43 +0200

    Argparse help supports placeholders allowed by argparse

--- tests/argparse/test_flag.py
+++ tests/argparse/test_flag.py
@@ -143,6 +143,20 @@ def test_help(empty_parser, autocomplete_and_compare, help):
         empty_parser, [r'(-a){{-a}}+[{}]:: :_files'.format(help_as_str)])
 
 
+def test_formatted_help(empty_parser, autocomplete_and_compare):
+    empty_parser.add_argument('-a', nargs='?', const='const', default='default',
+                              type=str, choices=['choice1', 'choice2'],
+                              required=True, metavar='metavar',
+                              help='%(nargs)s %(const)s %(default)s %(type)s '
+                              '%(choices)s %(required)s %(metavar)s %(dest)s '
+                              '%(help)s')
+    autocomplete_and_compare(empty_parser, [
+        r"(-a){-a}+[? const default <class 'str'> ['choice1', 'choice2'] True "
+        r'metavar a %(nargs)s %(const)s %(default)s %(type)s %(choices)s '
+        r'%(required)s %(metavar)s %(dest)s %(help)s]: :(choice1 choice2)'
+    ])
+
+
 def test_metavar(empty_parser, autocomplete_and_compare):
     empty_parser.add_argument('-a', metavar='name')
     autocomplete_and_compare(empty_parser, [r'(-a){-a}+:: :_files'])
