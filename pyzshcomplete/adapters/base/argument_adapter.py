89b865c9b6ac376a508665b0ee55cc25129401dd
dan1994 <dan1994@gmail.com>
2020-03-26 18:38:30 +0200

    Revert "Refactored subargument location to subargument separator and added all options as constants"
    This reverts commit 5d3d54f47b429e47e19c65cd99fd7b1d9aa8d1fe.

--- pyzshcomplete/adapters/base/argument_adapter.py
+++ pyzshcomplete/adapters/base/argument_adapter.py
@@ -49,11 +49,7 @@ class ArgumentAdapter(metaclass=ABCMeta):
 
 class StringifyableArgumentAdapter(ArgumentAdapter):
 
-    SUBARGUMENT_SPACE_SEPERATOR = ''
-    SUBARGUMENT_NO_SEPARATOR = '-'
-    SUBARGUMENT_EITHER_NO_OR_SPACE_SEPARATOR = '+'
-    SUBARGUMENT_EITHER_EQUAL_SIGN_OR_SPACE_SEPERATOR = '='
-    SUBARGUMENT_EITHER_EQUAL_SIGN_SEPERATOR = '=-'
+    SUBARGUMENT_LOCATION = '+'
 
     def __str__(self):
         if self.is_optional:
@@ -61,11 +57,11 @@ class StringifyableArgumentAdapter(ArgumentAdapter):
         return self._positional_argument_to_string()
 
     def _optional_argument_to_string(self):
-        return '{exclusion_list}{options}{subargument_separator}{help}' \
+        return '{exclusion_list}{options}{subargument_location}{help}' \
             '{subarguments}'.format(
                 exclusion_list=self._exclusion_list_to_string(),
                 options=self._options_to_string(),
-                subargument_separator=self._subargument_separator_to_string(),
+                subargument_location=self._subargument_location_to_string(),
                 help=self._help_to_string(),
                 subarguments=self._subarguments_to_string()
             )
@@ -91,9 +87,9 @@ class StringifyableArgumentAdapter(ArgumentAdapter):
     def _options_to_string(self):
         return '{{{}}}'.format(','.join(self.options))
 
-    def _subargument_separator_to_string(self):
+    def _subargument_location_to_string(self):
         if self.subargument_count == 1:
-            return StringifyableArgumentAdapter.SUBARGUMENT_EITHER_NO_OR_SPACE_SEPARATOR
+            return StringifyableArgumentAdapter.SUBARGUMENT_LOCATION
         return ''
 
     def _help_to_string(self):
