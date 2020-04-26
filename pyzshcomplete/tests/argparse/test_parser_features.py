1fd793e0424148846b983cff360c790c188bd828
Dan Arad <dan1994@gmail.com>
2020-04-26 20:35:45 +0300

    Added support for mutually exclusive options (in general and in argparse)

--- pyzshcomplete/tests/argparse/test_parser_features.py
+++ pyzshcomplete/tests/argparse/test_parser_features.py
@@ -10,3 +10,23 @@ def test_parents(autocomplete_and_compare):
         r':arg:_files',
         r'(-a)-a+: :_files'
     ])
+
+
+def test_groups(empty_parser, autocomplete_and_compare):
+    empty_parser.add_argument('arg_a')
+    group = empty_parser.add_argument_group()
+    group.add_argument('arg_b')
+    autocomplete_and_compare(empty_parser, [
+        r':arg_a:_files',
+        r':arg_b:_files'
+    ])
+
+
+def test_mutually_exclusive_groups(empty_parser, autocomplete_and_compare):
+    group = empty_parser.add_mutually_exclusive_group()
+    group.add_argument('-a')
+    group.add_argument('-b', action='append')
+    autocomplete_and_compare(empty_parser, [
+        r'(-a -b)-a+: :_files',
+        r'(-a)*-b+: :_files'
+    ])
